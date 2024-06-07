7917329abdab84b5024c7a1f49c2ea06a91b63c9
jianing <ning94mw@gmail.com>
2024-06-07 18:00:37 +0800

    profile setup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
-  def index
-    @user = User.new
+  def profile
+    @user = current_user
   end
 
-  def create
-    @user = User.new(user_params)
-    if user.save
-      redirect_to users_path
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    # user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, alert: "Profile updated"
+    else
+      render :profile, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:age, :gender, :fitness_goal, :height, :weight)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   has_many :plans
-  GENDER = %w[Male Female]
-  FITNESS_GOAL = %w[gain-muscle loss-weight]
+  GENDER = %w[Male Female].freeze
+  FITNESS_GOAL = %w[gain-muscle loss-weight].freeze
+  # AGE = %w[0..99].freeze
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :age, :height, :weight, presence: true
+  validates :age, presence: true, numericality: { in: 0..99, message: 'must be in 0 to 99' }
+  validates :height, :weight, presence: true
   validates :gender, presence: true, inclusion: { in: GENDER }
   validates :fitness_goal, presence: true, inclusion: { in: FITNESS_GOAL }
 end
