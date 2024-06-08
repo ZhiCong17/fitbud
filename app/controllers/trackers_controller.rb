5c12e5647e564aaea8c9c676100494eea114ca59
ZhiCong Ore <zhicong1989@gmail.com>
2024-06-08 19:26:51 +0800

    create tracker instance after submitting tracker form

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,2 +1,19 @@
 class TrackersController < ApplicationController
+  def create
+    @tracker = Tracker.new(tracker_params)
+    exercise_plan = ExercisePlan.find(params[:exercise_plan_id])
+    @tracker.exercise_plan = exercise_plan
+
+    if @tracker.save
+      redirect_to plan_exercise_plan_path(exercise_plan.plan, exercise_plan)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def tracker_params
+    params.require(:tracker).permit(:weight, :repetitions)
+  end
 end
