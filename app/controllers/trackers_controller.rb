baf060116d6c8c011da8a0584a327cdfa1667bef
ZhiCong Ore <zhicong1989@gmail.com>
2024-06-10 17:06:21 +0800

    limit tracker sets input to exercise plan sets

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,13 +1,19 @@
 class TrackersController < ApplicationController
   def create
     @tracker = Tracker.new(tracker_params)
-    exercise_plan = ExercisePlan.find(params[:exercise_plan_id])
-    @tracker.exercise_plan = exercise_plan
+    @exercise_plan = ExercisePlan.find(params[:exercise_plan_id])
+    @next_exercise_plan = ExercisePlan.find(params[:exercise_plan_id].to_i + 1)
+    @tracker.exercise_plan = @exercise_plan
 
     if @tracker.save
-      redirect_to plan_exercise_plan_path(exercise_plan.plan, exercise_plan)
+      if @exercise_plan.complete_set?
+        # need to redirect to next exercise
+        redirect_to plan_exercise_plan_path(@exercise_plan.plan, @next_exercise_plan)
+      else
+        redirect_to plan_exercise_plan_path(@exercise_plan.plan, @exercise_plan)
+      end
     else
-      redirect_to plan_exercise_plan_path(exercise_plan.plan, exercise_plan), alert: "Inputs cannot be empty"
+      render 'exercise_plans/show', status: :unprocessable_entity
     end
   end
 
--- app/models/exercise_plan.rb
+++ app/models/exercise_plan.rb
@@ -2,4 +2,8 @@ class ExercisePlan < ApplicationRecord
   belongs_to :plan
   belongs_to :exercise
   has_many :trackers
+
+  def complete_set?
+    set == trackers.count
+  end
 end
