1c0fbf918f3cf76134720100d0a065025031454c
weiyang <weiyang_11@outlook.com>
2024-06-11 15:26:14 +0800

    add on the function to limit the exercise create when the exercise already persisted

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,25 +1,22 @@
 require 'json'
 
 class PlansController < ApplicationController
+
+  def index
+  end
+
   def show
     plan = Plan.find(params[:id])
     @exercise_plans = plan.find
     @exercise_plans_by_day = @exercise_plans.group_by(&:suggested_day)
   end
 
-  def new
+  def create
     user = current_user
-    client = OpenAI::Client.new
-    chaptgpt_response = client.chat(parameters: {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "Give me a 1 week gym workout plan for a beginner. The plan should be for a #{user.age} year #{user.gender}, #{user.weight}kg, #{user.height}cm, looking to #{user.fitness_goal}. breakdown the plan in terms of days just present by integer like 1,2,3....(dont want show as day format). Each day could consist of own description and multiple exercises, each exercise can be broken down into one instance which are include name, sets, reps, weight show float, and/or duration. if the exercise requires, you can include a rest time. format your output into a json response"}]
-    })
-    @content = chaptgpt_response["choices"][0]["message"]["content"]
-    workout_plan = JSON.parse(@content)
-
+    plan = Plan.new()
+    workout_plan = plan.create(user)
     if plan = WorkoutPlanService.create_plan(user.id, workout_plan)
-      redirect_to plan_path(plan.id), alert: "New Workout plan created"
+      redirect_to plan_path(plan), alert: "New Workout plan created"
     end
   end
-
 end
--- app/services/workout_plan_service.rb
+++ app/services/workout_plan_service.rb
@@ -7,6 +7,7 @@ class WorkoutPlanService
     workout_plan.each do |days, details|
       order_num = 1
       description = details.first[1]
+      # description = details["description"]
       exercises = details["exercises"]
 
         if exercises.nil?
@@ -18,6 +19,9 @@ class WorkoutPlanService
           )
         else
           exercises.each do |exercise_details|
+          exercise_file = Exercise.find_by(title: exercise_details["name"])
+
+          if exercise_file.nil?
           exercise = Exercise.create(
               title: exercise_details["name"],
               description: "", # If no description provided, set to an empty string
@@ -38,6 +42,21 @@ class WorkoutPlanService
           order_num += 1
 
           exercise.update!(exercise_plan_id: exercise_plan.id)
+          else
+            exercise_plan = ExercisePlan.create!(
+              description: description,
+              order: order_num,
+              set: exercise_details["sets"],
+              repetitions: exercise_details["reps"],
+              suggested_day: days,
+              weight: exercise_details["weight"] || 0.0, # If no weight provided, set to 0.0
+              rest: exercise_details["rest"] || "", # If no rest provided, set to an empty string
+              duration: exercise_details["duration"] || 0, # If no duration provided, set to 0
+              exercise: exercise_file,
+              plan: plan
+              )
+            order_num += 1
+          end
           end
         end
     end
