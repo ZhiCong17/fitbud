271810560a3af0dc8ea5951bb7dc273f966f807b
weiyang <weiyang_11@outlook.com>
2024-06-11 14:32:00 +0800

    fix the bug for the rest day exercise plan missing.

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -12,7 +12,7 @@ class PlansController < ApplicationController
     client = OpenAI::Client.new
     chaptgpt_response = client.chat(parameters: {
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "Give me a 1 week gym workout plan for a beginner. The plan should be for a #{user.age} year #{user.gender}, #{user.weight}kg, #{user.height}cm, looking to #{user.fitness_goal}. breakdown the plan in terms of days just present by integer like 1,2,3....(dont want show as day format). Each day could consist of own description and multiple exercises, each exercise can be broken down into one instance which are include name, sets, reps, required weight show float, and/or duration. if the exercise requires, you can include a rest time. format your output into a json response"}]
+      messages: [{ role: "user", content: "Give me a 1 week gym workout plan for a beginner. The plan should be for a #{user.age} year #{user.gender}, #{user.weight}kg, #{user.height}cm, looking to #{user.fitness_goal}. breakdown the plan in terms of days just present by integer like 1,2,3....(dont want show as day format). Each day could consist of own description and multiple exercises, each exercise can be broken down into one instance which are include name, sets, reps, weight show float, and/or duration. if the exercise requires, you can include a rest time. format your output into a json response"}]
     })
     @content = chaptgpt_response["choices"][0]["message"]["content"]
     workout_plan = JSON.parse(@content)
