99acd2193731be3b91bdc93c371eb07b8bd13533
weiyang <weiyang_11@outlook.com>
2024-06-16 00:39:00 +0800

    able to the day card, grey color text if the after the current_day.

--- app/javascript/controllers/autofocus_controller.js
+++ app/javascript/controllers/autofocus_controller.js
@@ -2,8 +2,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static target = ["collapseElement"]
+
   connect() {
+    this.element.focus();
+
+  }
+
+  fire(){
     this.element.focus();
     console.log("hi")
   }
+
+      // Remove focus from other elements
 }
