81bae362b4429ee39614033167186ce1d6b37814
weiyang <weiyang_11@outlook.com>
2024-06-16 16:03:56 +0800

    fix the autofocus bug

--- app/javascript/controllers/autofocus_controller.js
+++ app/javascript/controllers/autofocus_controller.js
@@ -2,12 +2,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
-   connect() {
-    console.log(this.element)
-     setTimeout(() => {
-       this.element.focus();
-     });
-
-   }
+  static values = { currentDay: Number }
+  connect() {
+    const index = parseInt(this.element.dataset.index);
+    if (index + 1 === this.currentDayValue) {
+      setTimeout(() => {
+              this.element.focus();
+            }, 100);
+    }
+  }
 }
